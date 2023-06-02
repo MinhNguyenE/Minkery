01fbd596f57cbc4931d16b49753a4dee9044334f
MinhNguyenE <135257916+MinhNguyenE@users.noreply.github.com>
2023-06-02 17:10:32 +0700

    Add files via upload

---
+++ func.js
@@ -0,0 +1,19 @@
+function showBox () {
+    document.getElementById('log-box').style.
+}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
+// Select total count
+
+const totalCount = document.getElementById("total-count");
+var count = 0;
+totalCount.innerHTML = count;
